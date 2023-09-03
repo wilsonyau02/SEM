0b39371156f0cbf055c0520218a02b792ab0ec93
wilsonyau02 <yauwilson02@gmail.com>
2023-09-03 20:37:24 +0800

    setup supabase

--- sem/src/App.js
+++ sem/src/App.js
@@ -1,4 +1,26 @@
-function App(){
+import { supabase } from "./supabase-client";
+import { useState, useEffect } from 'react';
+
+
+function App() {
+
+    const [value, setValue] = useState({});
+
+    useEffect(() => {
+
+        async function getData() {
+            const { data, error } = await supabase
+                .from('Testing')
+                .select('*')
+
+            console.log(data, error);
+            setValue(data);
+        }
+
+
+        getData();
+    }, []);
+
     return <h1>Hello World</h1>;
 }
 
