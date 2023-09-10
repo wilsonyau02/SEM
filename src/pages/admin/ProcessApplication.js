23f56e94669189e776ac778c8c42294383d45a70
wilsonyau02 <yauwilson02@gmail.com>
2023-09-10 14:15:56 +0800

    add logout button

--- src/pages/admin/ProcessApplication.js
+++ src/pages/admin/ProcessApplication.js
@@ -1,7 +1,23 @@
+import { Button } from "antd"
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthProvider";
+
 function ProcessApplication () {
+
+    const navigate = useNavigate();
+    const { signOut } = useAuth();
+
+
+    const  handleLogout = async () => {
+        await signOut();
+        navigate('/login');
+    }
+
     return (
         <div>
             <h1>Process Application</h1>
+
+            <Button type="primary" onClick={handleLogout}>Logout</Button>
         </div>
     )
 
