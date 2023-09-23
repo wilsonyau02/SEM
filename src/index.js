01ccd1b6f4ef52c1bc64664fd321eda5a8ce2223
wilsonyau02 <yauwilson02@gmail.com>
2023-09-23 22:46:59 +0800

    change back route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './context/AuthProvider';
 // import '../src/pages/authentication/auth.css'
 import './index.css';
@@ -9,11 +9,11 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
